8a0a104124af53441400a785700953243c16b6de
Yevhen <i422kz@gmail.com>
2024-03-20 19:46:29 +0200

    v 1.4

--- Assets/Sripts/GameManager.cs
+++ Assets/Sripts/GameManager.cs
@@ -3,10 +3,24 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.Escape))
+            Screen.SetResolution(680, 720, false);
+        if (Input.GetKey(KeyCode.Space))
+            Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, true);
+    }
+
     public void RestartScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
+    
+    //public void ResizeWindow()
+    //{
+    //    Screen.SetResolution(Screen.currentResolution.width - 300, Screen.currentResolution.height - 200, false);
+    //}
 
     public void Quit()
     {
