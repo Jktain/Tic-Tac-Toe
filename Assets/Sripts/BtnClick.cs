a7581730e79c12fc75ef140fd6455494bafda920
Yevhen <i422kz@gmail.com>
2024-03-20 16:29:48 +0200

    v 1.3

--- Assets/Sripts/BtnClick.cs
+++ Assets/Sripts/BtnClick.cs
@@ -1,23 +1,25 @@
 using UnityEngine.UI;
 using UnityEngine;
 
-public class BtnClick : MonoBehaviour
+public class BtnClick : MonoBehaviour 
 {
-    public Texture2D[] turnImgs; //
-    public GameObject moveImg;
+    public Texture2D[] turnImgs; // Масив з картинками хрестика і нулика 
+    public GameObject moveImg; // Об'єкт на сцені, що показує хто ходить наступний
 
 
     public void Move()
     {
         WinCondition.score++;
+        // Ім'я кнопок - індекси елемента масива, якому буде призначене значення 0, або 1 в залежності від того, хто ходить
         int y = int.Parse(gameObject.name.Substring(0, 1));
         int x = int.Parse(gameObject.name.Substring(1, 1));
         WinCondition.gameMatrix[y, x] = WinCondition.score % 2;
-        WinCondition.CheckWin(y, x);
+        if(WinCondition.score >= 5) // До п'ятого ходу неможливо виграти, оскільки на полі повинно бути як мінімум 3 хрестика і 2 нулика
+            WinCondition.CheckWin(y, x);
         TurnToMove();
     }
 
-    private void TurnToMove()
+    private void TurnToMove() // Функція, що визначає хто ходить наступний і присвоює картинку об'єкту сцени moveImg
     {
         int nextTurn = (WinCondition.score + 1) % 2;
         moveImg.GetComponent<Image>().sprite = Sprite.Create(turnImgs[nextTurn], new Rect(0, 0, turnImgs[nextTurn].width, turnImgs[nextTurn].height), Vector2.zero);
--- Assets/Sripts/WinCondition.cs
+++ Assets/Sripts/WinCondition.cs
@@ -4,7 +4,7 @@ public class WinCondition : MonoBehaviour
 {
 
     public static int[,] gameMatrix = new int[3, 3];
-    public static int score;
+    public static int score; 
     
     private void Start()
     {
@@ -19,14 +19,11 @@ public class WinCondition : MonoBehaviour
 
     public static void CheckWin(int y, int x)
     {
-        if (score >= 5 && score < 10)
-        {
-            if (gameMatrix[0, 0] == gameMatrix[1, 1] && gameMatrix[1, 1] == gameMatrix[2, 2]) GameOver.instance.GameWin("diag1", 0, gameMatrix[y, x]);
-            else if (gameMatrix[0, 2] == gameMatrix[1, 1] && gameMatrix[1, 1] == gameMatrix[2, 0]) GameOver.instance.GameWin("diag2", 0, gameMatrix[y, x]);
-            else if (gameMatrix[y, 0] == gameMatrix[y, 1] && gameMatrix[y, 1] == gameMatrix[y, 2]) GameOver.instance.GameWin("hor", y, gameMatrix[y, x]);
-            else if (gameMatrix[0, x] == gameMatrix[1, x] && gameMatrix[1, x] == gameMatrix[2, x]) GameOver.instance.GameWin("ver", x, gameMatrix[y, x]);
-            else if (score == 9) GameOver.instance.GameDraw();
-        }
+        if (gameMatrix[0, 0] == gameMatrix[1, 1] && gameMatrix[1, 1] == gameMatrix[2, 2]) GameOver.instance.GameWin("diag1", 0, gameMatrix[y, x]);
+        else if (gameMatrix[0, 2] == gameMatrix[1, 1] && gameMatrix[1, 1] == gameMatrix[2, 0]) GameOver.instance.GameWin("diag2", 0, gameMatrix[y, x]);
+        else if (gameMatrix[y, 0] == gameMatrix[y, 1] && gameMatrix[y, 1] == gameMatrix[y, 2]) GameOver.instance.GameWin("hor", y, gameMatrix[y, x]);
+        else if (gameMatrix[0, x] == gameMatrix[1, x] && gameMatrix[1, x] == gameMatrix[2, x]) GameOver.instance.GameWin("ver", x, gameMatrix[y, x]);
+        else if (score == 9) GameOver.instance.GameDraw();
     }
 
  
